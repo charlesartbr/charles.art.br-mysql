db56271c4d116760fd5dc604cfb065ac3cc38152
charlesartbr <e-mail@charles.art.br>
2018-01-21 15:56:05 -0200

    company table

--- company.sql
+++ company.sql
@@ -3,7 +3,6 @@
 CREATE TABLE `company` 
 ( 
     `CompanyId` INT UNSIGNED NOT NULL AUTO_INCREMENT, 
-    `Name` VARCHAR(100) NOT NULL, 
-    `Website` VARCHAR(200) NOT NULL, 
+    `Name` VARCHAR(100) NOT NULL,
     PRIMARY KEY (`CompanyId`)
 ) ENGINE = InnoDB;
