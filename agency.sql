755e28900b1490aa96bd54430e6db6b4330dc956
charlesartbr <e-mail@charles.art.br>
2018-01-21 09:43:09 -0200

    agency table

--- agency.sql
+++ agency.sql
@@ -5,6 +5,6 @@ CREATE TABLE `agency`
     `AgencyId` INT UNSIGNED NOT NULL AUTO_INCREMENT, 
     `Name` VARCHAR(100) NOT NULL, 
     `Icon` VARCHAR(100) NOT NULL, 
-    `Website` VARCHAR(1000) NOT NULL, 
+    `Website` VARCHAR(100) NOT NULL, 
     PRIMARY KEY (`AgencyId`)
 ) ENGINE = InnoDB;
